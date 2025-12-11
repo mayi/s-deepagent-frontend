2878cf7e8767f4fa3bcca02119e7bdb203fedfe7
Ma Yi <mayi@live.com>
2025-12-11 17:55:33 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
